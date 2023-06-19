321438563eedd29cf82c83d07326041bc06621bb
Aldy Ardiansyah <aldyardiansyah628@gmail.com>
2023-06-19 19:28:28 +0700

    initial commit

---
+++ src/Entity/ExplosionType.java
@@ -0,0 +1,7 @@
+package Entity;
+
+public enum ExplosionType {
+    STRAY,
+    MOST,
+    PLAYER
+}
---
+++ src/Entity/Missile.java
@@ -0,0 +1,31 @@
+package Entity;
+
+import java.awt.image.BufferedImage;
+
+public class Missile extends Entity {
+    private static BufferedImage missileSprite;
+    private static BufferedImage enemyMissileSprite;
+
+    public Missile() {
+        width = 3;
+        height = 20;
+        cwidth = 3;
+        cheight = 14;
+    }
+
+    public static BufferedImage getEnemyMissileSprite() {
+        return enemyMissileSprite;
+    }
+
+    public static void setEnemyMissileSprite(final BufferedImage sprite) {
+        enemyMissileSprite = sprite;
+    }
+
+    public static BufferedImage getMissileSprite() {
+        return missileSprite;
+    }
+
+    public static void setMissileSprite(final BufferedImage sprite) {
+        missileSprite = sprite;
+    }
+}
